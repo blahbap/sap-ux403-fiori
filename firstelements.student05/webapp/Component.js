446c3a7abdb7fc0a8fab21593f56495bd8684f7d
Frank Stødle <fstod@equinor.com>
2022-11-30 13:13:14 +0000

    Initial commit

---
+++ firstelements.student05/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("firstelements.student05.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
